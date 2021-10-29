d95026cc071ddf04146b24d93a4360f69b5b87a3
"Shreyas" <"shreyas.vat@gmail.com">
2021-10-28 22:35:09 -0300

    fund,withdraw

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -2,14 +2,28 @@
 
 pragma solidity >=0.6.0 <0.9.0;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
 contract FundMe {
+    using SafeMathChainlink for uint256;
+    address public owner;
+    
+    constructor () public{
+        owner=msg.sender;
+    } 
     
     mapping(address =>uint256) public addressToAmoundFunded;
     function fund() public  payable {
+        uint256 minimumUSD=1*10**18;
+        require(getCoversionRate(msg.value)>=minimumUSD, "You need to spend more eth");
         addressToAmoundFunded[msg.sender] += msg.value;
         //what the ETH -> USD conversion rate
         
+    }
+    
+    function withdraw() payable public{
+        require(msg.sender == owner);
+        msg.sender.transfer(address(this).balance);
     }
     function getVersion() public view returns(uint256){
         AggregatorV3Interface priceFeed =AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
@@ -18,13 +32,14 @@ contract FundMe {
     
     function getPrice() public view returns(uint256){
         AggregatorV3Interface priceFeed =AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
-         (
-            uint80 roundID, 
-            int price,
-            uint startedAt,
-            uint timeStamp,
-            uint80 answeredInRound
-        ) = priceFeed.latestRoundData();
-        return uint256(price);
+         (,int price,,,) = priceFeed.latestRoundData();
+        return uint256(price*1000000000);
+    }
+    
+    function getCoversionRate(uint256 ethAmount) public view returns(uint256){
+        uint256 ethPrice =getPrice();
+        uint256 ethAmountInUsd =(ethPrice *ethAmount)/1000000000000000000;
+        return ethAmountInUsd;
+        
     }
 }
